cc30dcd28849b439512a613c257645db30b06966
tkgaolol <tkgao@connect.ust.hk>
2024-07-08 10:40:28 +0800

    ocr

--- prod/pages/ocr_home.py
+++ prod/pages/ocr_home.py
@@ -7,6 +7,7 @@ import time
 import cv2
 from io import BytesIO
 import numpy as np
+import streamlit.components.v1 as components
 
 st.session_state['cur_page'] = '掃描'
 
@@ -55,23 +56,33 @@ if video_capture is not None:
     # Initialize sound output driver
     # engine = pyttsx3.init(driverName="nsss")  # mac
     # engine = pyttsx3.init(driverName="sapi5")  # winows
-    engine = pyttsx3.init(driverName="espeak")  # linux version
-    voices = engine.getProperty('voices')
+    # engine = pyttsx3.init(driverName="espeak")  # linux version
+    # voices = engine.getProperty('voices')
 
-    for voice in voices:
-        if 'ZH-HK' in voice.id.split('\\')[-1]:
-            engine.setProperty('voice', voice.id)
-            break
+    # for voice in voices:
+    #     if 'ZH-HK' in voice.id.split('\\')[-1]:
+    #         engine.setProperty('voice', voice.id)
+    #         break
 
-    engine.setProperty('rate',150 )
-    engine.setProperty('volume',1)
+    # engine.setProperty('rate',150 )
+    # engine.setProperty('volume',1)
     
     # Display the extracted text
     for r in result:
         st.write(r[1])
-        engine.say(r[1])
+        # engine.say(r[1])
             # st.page_link(page= title_url, label= title)
             # st.markdown(title_url)
-            
-        engine.runAndWait()
+        components.html(
+			f"""
+			<script>
+			var msg = new SpeechSynthesisUtterance("{r[1]}");
+			msg.rate = 0.75;
+			msg.volume = 1;
+			window.speechSynthesis.speak(msg);
+			</script>
+			"""
+			, height=0, width=0
+		)
+        # engine.runAndWait()
         time.sleep(2)
